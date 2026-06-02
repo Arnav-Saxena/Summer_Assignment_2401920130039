22bc529058b42adffe1a76676fbc6f998887351e
Arnav Saxena <arnavsaxena.work@gmail.com>
2026-06-02 17:36:58 +0530

    Create MaximumSubArray.java

---
+++ Week 1/Day 2/MaximumSubArray.java
@@ -0,0 +1,17 @@
+class MaximumSubArray
+{
+    public int maxSubArray(int[] nums) 
+    {
+        int sum = 0, maxSum = Integer.MIN_VALUE;
+        for(int num: nums)
+        {
+            sum = sum+num;
+            maxSum = Math.max(sum, maxSum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxSum;
+    }
+}
