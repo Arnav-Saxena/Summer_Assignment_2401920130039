38164202fe00006138c741b1f79812ec72712f52
Arnav Saxena <arnavsaxena.work@gmail.com>
2026-06-11 14:04:56 +0530

    Update MinStack.java

--- Week 2/Day 3/MinStack.java
+++ Week 2/Day 3/MinStack.java
@@ -1,29 +1,43 @@
-class MinStack {
-
-    private Stack<int[]> s;
-
-    public MinStack() {
-        s = new Stack<>();
-    }
-
-    public void push(int val) {
-        if (s.isEmpty()) {
-            s.push(new int[]{val, val});
-        } else {
-            int minVal = Math.min(val, s.peek()[1]);
-            s.push(new int[]{val, minVal});
+import java.lang.Math.*;
+import java.util.Stack;
+class MinStack 
+{
+    Stack<int[]> stack = new Stack<>();
+    
+    public void push(int val) 
+    {
+        if(stack.isEmpty())
+        {
+            stack.push(new int[]{val, val});
+        }
+        else
+        {
+            int minValue = Math.min(stack.peek()[1], val);
+            stack.push(new int[]{val, minValue});
         }
     }
-
-    public void pop() {
-        s.pop();
+    
+    public void pop() 
+    {
+        stack.pop();
     }
-
-    public int top() {
-        return s.peek()[0];
+    
+    public int top() 
+    {
+        return stack.peek()[0];
     }
-
-    public int getMin() {
-        return s.peek()[1];
+    
+    public int getMin() 
+    {
+        return stack.peek()[1];
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
