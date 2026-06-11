fea8a449824ae164e8981003f5fbbbf24be25010
Arnav Saxena <arnavsaxena.work@gmail.com>
2026-06-11 14:07:24 +0530

    Create NextParentheses.java

---
+++ Week 2/Day 3/NextParentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+class NextParentheses
+{
+    public boolean isValid(String s) 
+    {
+        if (s.length() % 2 != 0)
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) 
+        {
+            if (ch == '(' || ch == '[' || ch == '{') 
+            {
+                stack.push(ch);
+            } 
+            else 
+            {
+                if (stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
