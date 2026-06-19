5f95c3f270f2bf698d3270f25dc50cc2d6720cff
Arnav Saxena <arnavsaxena.work@gmail.com>
2026-06-19 22:15:38 +0530

    .

--- Week 2/Day 2/ListNode.java
+++ Week 2/Day 2/ListNode.java
@@ -1,14 +1,13 @@
-import java.util.*;
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+
+  //Definition for singly-linked list.
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class MergeTwoSortedLists
 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) 
