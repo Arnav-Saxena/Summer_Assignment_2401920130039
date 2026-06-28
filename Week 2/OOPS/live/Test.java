c4fb3c3b0d9bfb3129628901cabcec0dc812190d
Arnav Saxena <arnavsaxena.work@gmail.com>
2026-06-28 11:19:56 +0530

    oops

---
+++ Week 2/OOPS/live/Test.java
@@ -0,0 +1,26 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        // a. Create an instance of Veena and call play()
+        Veena veena = new Veena();
+        veena.play();
+
+        // b. Create an instance of Saxophone and call play()
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        // c. Place instances in a variable of type Playable and call play()
+        Playable playableInstrument;
+
+        playableInstrument = veena;
+        playableInstrument.play();
+
+        playableInstrument = saxophone;
+        playableInstrument.play();
+    }
+}
